27b1c5bb9c09f9c1941f3a5d920cd829d46688fa
SimonCrA <csimonr28@gmail.com>
2019-07-04 15:54:41 -0400

    Esp32 used like a beacon eddystone-UID

---
+++ src/Beacon.cpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2019 Sho Kuroda <krdlab@gmail.com>
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <Arduino.h>
+#include <eddystone.hpp>
+
+
+const std::string NAMESPACE = "AABBCCDDEEFF12345677";
+const std::string INSTANCE  = "000000000003";
+
+void setBeacon(BLEAdvertising* pAdvertising) {
+  EddystoneUid uid(NAMESPACE, INSTANCE);
+  BLEAdvertisementData adData;
+  uid.compose(adData);
+  pAdvertising->setAdvertisementData(adData);
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(10);
+  }
+  
+  
+  BLEDevice::init("");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  setBeacon(pAdvertising);
+
+  Serial.println("Advertising for 1 seccond only...");
+  pAdvertising->start();
+  delay(1000);
+  pAdvertising->stop();
+  Serial.println("Advertising stopped.");
+
+}
+
+void loop() {
+  //delay(1000);
+
+
+}
